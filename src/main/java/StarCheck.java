aed938820c466bfee3bb6ea8dfe2d8499e7ea8d2
CodingPenguin <email@example.com>
2020-11-01 16:35:27 -0600

    adding api calls

--- src/main/java/StarCheck.java
+++ src/main/java/StarCheck.java
@@ -1,2 +1,15 @@
-public class StarCheck {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarCheck extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info(getDescription().getName() + " was enabled!");
+
+        getCommand("starcheck").setExecutor(new dantoc.commands.StarCheck());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(getDescription().getName() + " was disabled!");
+    }
 }
---
+++ src/main/java/dantoc/api/Util.java
@@ -0,0 +1,9 @@
+package dantoc.api;
+
+import net.hypixel.api.HypixelAPI;
+import java.util.UUID;
+
+public class Util {
+    public static final HypixelAPI API = new HypixelAPI(UUID.fromString("0ca88a8f-3a5b-43c2-8a00-1a588ae49e42")); // arbitrary key, replace with your own to test
+    public static final UUID HYPIXEL = UUID.fromString("bf3d281d-edca-456a-a1a3-865e60201375"); // RetlocPeck's UUID
+}
