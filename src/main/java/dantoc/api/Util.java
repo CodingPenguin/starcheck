9af9ea1070197a86caff1f30857ee00f6a758535
CodingPenguin <email@example.com>
2020-11-01 16:46:26 -0600

    merged and api calls

--- src/main/java/dantoc/api/Util.java
+++ src/main/java/dantoc/api/Util.java
@@ -4,6 +4,6 @@ import net.hypixel.api.HypixelAPI;
 import java.util.UUID;
 
 public class Util {
-    public static final HypixelAPI API = new HypixelAPI(UUID.fromString("0ca88a8f-3a5b-43c2-8a00-1a588ae49e42")); // arbitrary key, replace with your own to test
+    public static final HypixelAPI API = new HypixelAPI(UUID.fromString("64bd424e-ccb0-42ed-8b66-6e42a135afb4")); // arbitrary key, replace with your own to test
     public static final UUID HYPIXEL = UUID.fromString("bf3d281d-edca-456a-a1a3-865e60201375"); // RetlocPeck's UUID
 }
